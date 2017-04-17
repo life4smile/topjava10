048e58fa9644f07ab0787a3a9330831243a02918
life4smile <xobbut07@gmail.com>
2017-04-17 20:04:37 +0300

    HM03

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -1,37 +1,98 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.DateTimeUtil;
 
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class JdbcMealRepositoryImpl implements MealRepository {
 
+    private static final RowMapper<Meal> ROW_MAPPER = new RowMapper<Meal>() {
+        @Override
+        public Meal mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new Meal(rs.getInt("id"), rs.getTimestamp("datetime").toLocalDateTime(),
+                    rs.getString("description"), rs.getInt("calories"));
+        }
+    };
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private final SimpleJdbcInsert insertMeal;
+
+    @Autowired
+    public JdbcMealRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.insertMeal = new SimpleJdbcInsert(dataSource)
+                .withTableName("meals")
+                .usingGeneratedKeyColumns("id");
+
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("user_id", userId)
+                .addValue("id", meal.getId())
+                .addValue("datetime", meal.getDateTime())
+                .addValue("description", meal.getDescription())
+                .addValue("calories", meal.getCalories());
+
+        if (meal.isNew())
+        {
+            Number newKey = insertMeal.executeAndReturnKey(map);
+            meal.setId(newKey.intValue());
+        }
+        else
+        {
+            namedParameterJdbcTemplate.update("UPDATE meals SET datetime=:datetime, " +
+                    "description=:description, calories=:calories  WHERE id=:id", map);
+        }
+
+        return meal;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return jdbcTemplate.update("DELETE FROM meals WHERE id=?", id) != 0;
     }
 
     @Override
-    public Meal get(int id, int userId) {
-        return null;
+    public Meal get(int id, int userId)
+    {
+        List<Meal> meals = jdbcTemplate.query("SELECT * FROM meals WHERE id=?", ROW_MAPPER, id);
+        return DataAccessUtils.singleResult(meals);
     }
 
     @Override
-    public List<Meal> getAll(int userId) {
-        return null;
+    public List<Meal> getAll(int userId)
+    {
+        return jdbcTemplate.query("SELECT * FROM meals WHERE  user_id=? ORDER BY datetime DESC", ROW_MAPPER, userId);
     }
 
     @Override
-    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId)
+    {
+        List<Meal> meals = jdbcTemplate.query("SELECT * FROM meals WHERE user_id=?", ROW_MAPPER, userId);
+        return meals.stream()
+                .filter(meal -> DateTimeUtil.isBetween(meal.getDateTime(), startDate, endDate))
+                .collect(Collectors.toList());
     }
 }
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -11,3 +12,10 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, id, datetime, description, calories) VALUES
+ (100001, 1, '2015-05-30 14:00', 'Админ ланч', 510),
+ (100001, 2, '2015-05-30 21:00', 'Админ ужин', 510),
+ (100000, 3, '2015-05-31 10:00', 'Завтрак', 500),
+ (100000, 4, '2015-05-31 14:00', 'Обед', 1000),
+ (100000, 5, '2015-05-31 18:00', 'Ужин', 500);
